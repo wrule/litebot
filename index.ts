777c77f3cfeec3afe3db51ba8ecfe3672e2bdda7
jimao <jimao@jimaodeMBP.lan>
2022-11-12 21:19:38 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
+import { SMACross } from './bot/sma_cross';
+import { ArrayToKLine } from './tc/ohlcv';
+import moment from 'moment';
+const data = require('./data/ETH_USDT-30m.json');
 
-console.log('你好，世界');
+async function main() {
+  const kline = ArrayToKLine(data);
+  const bot = new SMACross({ queue_size: 100, params: { fast_period: 9, slow_period: 44 } });
+  kline.forEach((ohlcv, index) => {
+    console.log(index, moment(new Date(ohlcv.time)).format('YYYY-MM-DD HH:mm:ss'));
+    bot.Update(ohlcv);
+  });
+}
+
+main();
+
