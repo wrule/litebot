5b22db9025e926a017160e08a68c2e13e663a28f
jimao <jimao@jimaodeMBP.lan>
2022-11-12 18:46:53 +0800

    丰富

---
+++ bot/sma_cross.ts
@@ -0,0 +1,18 @@
+import { Bot } from '.';
+import { TC } from '../tc';
+
+export
+class SMACross
+extends Bot<TC, TC, TC> {
+  protected calculate(tc: TC, state_queue: TC[]) {
+    return null as any;
+  }
+
+  protected analyze(state_queue: TC[]) {
+    return null as any;
+  }
+
+  protected execute(signal: TC) {
+
+  }
+}
