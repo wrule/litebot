8cc868d8dff1d2feb8ae49a7b0af40f18ee1d8ab
jimao <jimao@jimaodeMBP.lan>
2022-11-12 18:48:37 +0800

    提交代码

--- bot/sma_cross.ts
+++ bot/sma_cross.ts
@@ -2,17 +2,25 @@ import { Bot } from '.';
 import { TC } from '../tc';
 
 export
+interface State
+extends TC {
+  sma_fast: number;
+  sma_slow: number;
+  diff: number;
+}
+
+export
 class SMACross
-extends Bot<TC, TC, TC> {
-  protected calculate(tc: TC, state_queue: TC[]) {
+extends Bot<TC, State, State> {
+  protected calculate(tc: TC, state_queue: State[]) {
     return null as any;
   }
 
-  protected analyze(state_queue: TC[]) {
+  protected analyze(state_queue: State[]) {
     return null as any;
   }
 
-  protected execute(signal: TC) {
+  protected execute(signal: State) {
 
   }
 }
