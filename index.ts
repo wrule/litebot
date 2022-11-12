0a97901261919f231e60f7e3ac82200424c5c44d
jimao <jimao@jimaodeMBP.lan>
2022-11-12 21:45:56 +0800

    测时间

--- index.ts
+++ index.ts
@@ -6,7 +6,9 @@ const data = require('./data/ETH_USDT-30m.json');
 async function main() {
   const kline = ArrayToKLine(data);
   const bot = new SMACross({ queue_size: 100, params: { fast_period: 9, slow_period: 44 } });
+  const old_time = Number(new Date());
   bot.BackTesting(kline);
+  console.log(Number(new Date()) - old_time);
 }
 
 main();
