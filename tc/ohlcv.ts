fb9924d7702c6536b4ce72433a843b41f68dbec3
jimao <jimao@jimaodeMBP.lan>
2022-11-12 21:05:27 +0800

    数据功能

--- tc/ohlcv.ts
+++ tc/ohlcv.ts
@@ -5,3 +5,20 @@ interface OHLCV
 extends OHLC {
   volume: number;
 }
+
+export
+function ArrayToOHLCV(array: number[]): OHLCV {
+  return {
+    time: array[0],
+    open: array[1],
+    high: array[2],
+    low: array[3],
+    close: array[4],
+    volume: array[5],
+  };
+}
+
+export
+function ArrayToKLine(array: number[][]): OHLCV[] {
+  return array.map((item) => ArrayToOHLCV(item));
+}
