3e723af2f6fdc47c4da4190e70b1d7b4e4b690d5
jimao <jimao@jimaodeMBP.lan>
2022-11-12 18:32:00 +0800

    计算,分析,执行框架

--- bot/index.ts
+++ bot/index.ts
@@ -1,9 +1,21 @@
 import { TC } from '../tc';
 
 export
-class Bot<
+abstract class Bot<
   T extends TC,
-  State
+  State extends T,
+  Signal extends State
 > {
+  private state_queue: State[] = [];
 
+  public Update(tc: T) {
+    const new_state = this.calculate(tc, this.state_queue);
+    this.state_queue.push(new_state);
+  }
+
+  protected abstract calculate(tc: T, state_queue: State[]): State;
+
+  protected abstract analyze(state_queue: State[]): Signal;
+
+  protected abstract execute(signal: Signal): void;
 }
