c2f5572e960c7b4b87b2004f4e4a8e61d225e978
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-14 17:17:34 +0800

    提交代码

--- watcher/kline_watcher.ts
+++ watcher/kline_watcher.ts
@@ -3,27 +3,31 @@ import { Exchange } from 'ccxt';
 export
 class KLineWatcher {
   private timer: any = null;
+  private keep = false;
 
   public async Start(
     exchange: Exchange,
     symbol: string,
     timeframe: string,
-    interval: number,
+    limit = 2,
+    interval = 1000,
   ) {
     try {
-      const kline = await exchange.fetchOHLCV(symbol, timeframe, undefined, 2);
+      this.keep = true;
+      const kline = await exchange.fetchOHLCV(symbol, timeframe, undefined, limit);
       console.log(kline);
     } catch (e) {
       console.log(e);
     } finally {
       clearTimeout(this.timer);
+      if (!this.keep) return;
       this.timer = setTimeout(() => {
-        this.timer && this.Start(exchange, symbol, timeframe, interval);
+        this.Start(exchange, symbol, timeframe, limit, interval);
       }, interval);
     }
   }
 
   public Stop() {
-    this.timer = null;
+    this.keep = false;
   }
 }
