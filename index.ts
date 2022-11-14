cdcc7858f7c0945208407cf5a631afc2316b585a
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-14 16:54:29 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -2,27 +2,32 @@ import { Params, SMACross } from './bot/sma_cross';
 import { Random } from './optimizer/random';
 import { ArrayToKLine } from './tc/ohlcv';
 const data = require('./data/ETH_USDT-2h.json');
+import { binance } from 'ccxt';
+import { KLineWatcher } from './watcher/kline_watcher';
 
 async function main() {
-  const kline = ArrayToKLine(data);
+  const watcher = new KLineWatcher();
+  watcher.Start(new binance(), 'ETH/USDT', '1m', 100);
 
-  const op = new Random<Params>();
-  op.Search({
-    domain: {
-      fast_period: [2, 100],
-      slow_period: [2, 100],
-    },
-    target: (params) => {
-      const bot = new SMACross({ params });
-      bot.BackTestingBatch(kline);
-      return bot.SimpleSpot.ROI(kline[kline.length - 1].close);
-    },
-    mapper: (value) => ({
-      fast_period: Math.min(value.fast_period, value.slow_period),
-      slow_period: Math.max(value.fast_period, value.slow_period),
-    }),
-    filter: (params) => Math.abs(params.slow_period - params.fast_period) > 5,
-  });
+  // const kline = ArrayToKLine(data);
+
+  // const op = new Random<Params>();
+  // op.Search({
+  //   domain: {
+  //     fast_period: [2, 100],
+  //     slow_period: [2, 100],
+  //   },
+  //   target: (params) => {
+  //     const bot = new SMACross({ params });
+  //     bot.BackTestingBatch(kline);
+  //     return bot.SimpleSpot.ROI(kline[kline.length - 1].close);
+  //   },
+  //   mapper: (value) => ({
+  //     fast_period: Math.min(value.fast_period, value.slow_period),
+  //     slow_period: Math.max(value.fast_period, value.slow_period),
+  //   }),
+  //   filter: (params) => Math.abs(params.slow_period - params.fast_period) > 5,
+  // });
 }
 
 main();
