7fd9c84aa9c505da93d9863a8877d4a7cf696350
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-14 12:53:29 +0800

    测试代码

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
-import { SMACross } from './bot/sma_cross';
+import { Params, SMACross } from './bot/sma_cross';
+import { Random } from './optimizer/random';
 import { ArrayToKLine } from './tc/ohlcv';
 const data = require('./data/ETH_USDT-2h.json');
 
 async function main() {
   const kline = ArrayToKLine(data);
-  const bot = new SMACross({ params: { fast_period: 9, slow_period: 44 } });
-  const old_time = Number(new Date());
-  bot.BackTestingBatch(kline);
-  console.log(Number(new Date()) - old_time);
-  console.log(bot.SimpleSpot.ROI(kline[kline.length - 1].close));
+
+  const op = new Random<Params>();
+  op.Search({
+    domain: {
+      fast_period: [2, 100],
+      slow_period: [2, 100],
+    },
+    target: (params) => {
+      const bot = new SMACross({ params });
+      bot.BackTestingBatch(kline);
+      return bot.SimpleSpot.ROI(kline[kline.length - 1].close);
+    },
+    params_mapper: (value) => ({
+      fast_period: Math.min(value.fast_period, value.slow_period),
+      slow_period: Math.max(value.fast_period, value.slow_period),
+    }),
+  });
 }
 
 main();
-
