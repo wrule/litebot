9202adf4a56e7af475a70d53558935e50292341f
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-14 12:40:16 +0800

    提交代码

--- optimizer/random.ts
+++ optimizer/random.ts
@@ -14,30 +14,30 @@ function RandomSelect(domain: Domain) {
   Object.entries(domain).forEach(([key, value]) => {
     const min = Math.min(...value);
     const max = Math.max(...value);
-    const diff = max - min;
+    const diff = max - min + 1;
     result[key] = Math.floor(min + diff * Math.random());
   });
   return result;
 }
 
 export
-interface Option<Param> {
+interface Option<Params> {
   domain: Domain,
-  target: (param: Param) => number;
-  param_mapper?: (value: Value) => Param;
-  param_filter?: (param: Param) => boolean;
-  sample?: (param: Param, result: number) => void;
+  target: (param: Params) => number;
+  param_mapper?: (value: Value) => Params;
+  param_filter?: (param: Params) => boolean;
+  sample?: (param: Params, result: number) => void;
 }
 
 export
-class Random {
-  public Search<Param>(options: Option<Param>) {
+class Random<Params> {
+  public Search(options: Option<Params>) {
     while (true) {
       const value = RandomSelect(options.domain);
-      const param = options.param_mapper ? options.param_mapper(value) : value as Param;
-      if (options.param_filter && !options.param_filter(param)) continue;
-      const result = options.target(param);
-      options.sample && options.sample(param, result);
+      const params = options.param_mapper ? options.param_mapper(value) : value as Params;
+      if (options.param_filter && !options.param_filter(params)) continue;
+      const result = options.target(params);
+      options.sample && options.sample(params, result);
     }
   }
 }
