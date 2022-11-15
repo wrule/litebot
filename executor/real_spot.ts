f9382b3d7985b5e8889b65e9918a5fd75bf421b7
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-15 14:40:01 +0800

    send message

--- executor/real_spot.ts
+++ executor/real_spot.ts
@@ -1,4 +1,4 @@
-import { Exchange } from 'ccxt';
+import { Exchange, Order } from 'ccxt';
 import moment from 'moment';
 import { Notifier } from '../notifier';
 
@@ -18,6 +18,17 @@ class RealSpot {
   private funds = 0;
   private assets = 0;
 
+  private send_message(order: Order, price: number, in_out: [number, number], order_time: string) {
+    this.config.notifier?.SendMessage(JSON.stringify({
+      time: moment(new Date(order.timestamp)).format('YYYY-MM-DD HH:mm:ss'),
+      symbol: order.symbol, side: order.side,
+      in_amount: in_out[0], out_amount: in_out[1],
+      expected_price: price, final_price: order.price, deviation: `${(order.price - price) / price * 100}%`,
+      funds: this.funds, assets: this.assets,
+      order_time,
+    }, null ,2));
+  }
+
   public async BuyAll(price: number) {
     const request_time = Number(new Date());
     const order = await this.config.exchange.createMarketOrder(
@@ -29,30 +40,26 @@ class RealSpot {
         quoteOrderQty: this.config.exchange.costToPrecision(this.config.symbol, this.funds),
       },
     );
-    const response_time = Number(new Date());
+    const order_time = `${(Number(new Date()) - request_time) / 1000}s`;
     const in_amount = order.cost;
     const out_amount = order.amount - (this.config.symbol.startsWith(order.fee.currency) ? order.fee.cost : 0);
     this.funds -= in_amount;
     this.assets += out_amount;
-    this.config.notifier?.SendMessage(JSON.stringify({
-      time: moment(new Date(order.timestamp)).format('YYYY-MM-DD HH:mm:ss'),
-      symbol: this.config.symbol, side: order.side,
-      in_amount, out_amount,
-      expected_price: price, final_price: order.price, deviation: `${(order.price - price) / price * 100}%`,
-      order_time: `${(response_time - request_time) / 1000}s`,
-      funds: this.funds, assets: this.assets,
-    }, null ,2));
+    this.send_message(order, price, [in_amount, out_amount], order_time);
   }
 
   public async SellAll(price: number) {
+    const request_time = Number(new Date());
     const order = await this.config.exchange.createMarketOrder(
       this.config.symbol,
       'sell',
       this.config.exchange.amountToPrecision(this.config.symbol, this.assets),
     );
+    const order_time = `${(Number(new Date()) - request_time) / 1000}s`;
     const in_amount = order.amount;
     const out_amount = order.cost - (this.config.symbol.endsWith(order.fee.currency) ? order.fee.cost : 0);
     this.assets -= in_amount;
     this.funds += out_amount;
+    this.send_message(order, price, [in_amount, out_amount], order_time);
   }
 }
