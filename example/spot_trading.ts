82e6721f15aa55997c6e637b13489f349df9b729
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-15 15:11:23 +0800

    例子

--- example/spot_trading.ts
+++ example/spot_trading.ts
@@ -1,19 +1,21 @@
 import { binance } from 'ccxt';
-import { RealSpot } from './executor/real_spot';
-import { DingTalk } from './notifier/dingtalk';
+import { RealSpot } from '../executor/real_spot';
+import { DingTalk } from '../notifier/dingtalk';
+
 const secret = require('./.secret.json');
-const dingtalk = require('./.dingtalk.json');
+const config = require('./.dingtalk.json');
 
 async function main() {
-  const notifier = new DingTalk(dingtalk);
+  const notifier = new DingTalk(config);
   const exchange = new binance({ ...secret, enableRateLimit: true });
   console.log('加载市场...');
   await exchange.loadMarkets();
   const spot = new RealSpot({ exchange, symbol: 'UNI/USDT', init_funds: 15, init_assets: 0, notifier });
-  console.log('发起交易...');
+  console.log('发起买交易...');
   await spot.BuyAll(5);
+  console.log('发起卖交易...');
   await spot.SellAll(5);
-  console.log('结束交易');
+  console.log('结束');
 }
 
 main();
