788ac2c2a9ec3d78788606996902428a3e50ff9f
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-15 12:32:57 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -6,8 +6,13 @@ async function main() {
   const exchange = new binance({ ...secret, enableRateLimit: true });
   console.log('加载市场...');
   await exchange.loadMarkets();
-  const spot = new RealSpot({ exchange, symbol: 'SHIB/USDT', init_funds: 0, init_assets: 1637704.66 });
+  const spot = new RealSpot({ exchange, symbol: 'SHIB/USDT', init_funds: 15, init_assets: 0 });
   console.log('发起交易...');
+  console.log(await spot.BuyAll());
+  console.log(await spot.SellAll());
+  console.log(await spot.BuyAll());
+  console.log(await spot.SellAll());
+  console.log(await spot.BuyAll());
   console.log(await spot.SellAll());
   console.log('结束交易');
 }
