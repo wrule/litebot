3dc7971748770853622f9a1d82e74183febff8c5
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-15 11:18:05 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,35 +1,11 @@
-import { Params, SMACross } from './bot/sma_cross';
-import { Random } from './optimizer/random';
-import { ArrayToKLine } from './tc/ohlcv';
-const data = require('./data/ETH_USDT-2h.json');
 import { binance } from 'ccxt';
-import { KLineWatcher } from './watcher/kline_watcher';
+import { RealSpot } from './executor/real_spot';
+const secret = require('./.secret.json');
 
 async function main() {
-  const watcher = new KLineWatcher();
-  watcher.Start(new binance(), 'ETH/USDT', '1m', 2, (kline) => {
-    console.log(kline);
-  });
-
-  // const kline = ArrayToKLine(data);
-
-  // const op = new Random<Params>();
-  // op.Search({
-  //   domain: {
-  //     fast_period: [2, 100],
-  //     slow_period: [2, 100],
-  //   },
-  //   target: (params) => {
-  //     const bot = new SMACross({ params });
-  //     bot.BackTestingBatch(kline);
-  //     return bot.SimpleSpot.ROI(kline[kline.length - 1].close);
-  //   },
-  //   mapper: (value) => ({
-  //     fast_period: Math.min(value.fast_period, value.slow_period),
-  //     slow_period: Math.max(value.fast_period, value.slow_period),
-  //   }),
-  //   filter: (params) => Math.abs(params.slow_period - params.fast_period) > 5,
-  // });
+  const exchange = new binance({ ...secret, enableRateLimit: true });
+  const spot = new RealSpot({ exchange, symbol: 'BTC/USDT', init_funds: 11 });
+  spot.BuyAll();
 }
 
 main();
