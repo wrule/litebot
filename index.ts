5a9aa65fb51503ec08086aeeead03cab4fb302c5
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-15 22:31:43 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -8,21 +8,15 @@ const secret = require('./.secret.json');
 const dingtalk = require('./.dingtalk.json');
 
 async function main() {
-  const notifier = new DingTalk(dingtalk);
   const exchange = new binance({ ...secret, enableRateLimit: true });
-  console.log('加载市场...');
   await exchange.loadMarkets();
   const executor = new SimpleSpot(100, 0.001);
   const bot = new SMACross(executor, { fast_period: 9, slow_period: 44 });
   const watcher = new KLineWatcher();
   (await watcher.Fetch(exchange, 'ETH/USDT', '1m', 100)).forEach((item) => bot.Update(item, false));
-  watcher.
-
-  // const spot = new RealSpot({ exchange, symbol: 'UNI/USDT', init_funds: 15, init_assets: 0, notifier });
-  // console.log('发起交易...');
-  // await spot.BuyAll(5);
-  // await spot.SellAll(5);
-  // console.log('结束交易');
+  watcher.Start(exchange, 'ETH/USDT', '1m', 2, (kline) => {
+    bot.Update(kline[0]);
+  }, 1000);
 }
 
 main();
