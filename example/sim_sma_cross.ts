4f80a535603d04cd08df5c5ce2e16f979e4fc4cc
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-17 17:47:13 +0800

    提交代码

---
+++ example/sim_sma_cross.ts
@@ -0,0 +1,15 @@
+import { binance } from 'ccxt';
+import { SMACross } from '../bot/sma_cross';
+import { SimpleSpot } from '../executor/simple_spot';
+import { KLineWatcher } from '../watcher/kline_watcher';
+
+async function main() {
+  const exchange = new binance({ });
+  console.log('loading market...');
+  await exchange.loadMarkets();
+  const executor = new SimpleSpot(100, 0.001);
+  const bot = new SMACross(executor, { fast_period: 9, slow_period: 44 });
+  new KLineWatcher().RunBot(exchange, 'ETH/USDT', '1m', bot);
+}
+
+main();
