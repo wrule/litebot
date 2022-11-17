cf0984309abaec189faf752d8100df0a728c7ea0
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-17 17:24:12 +0800

    提交代码

--- watcher/kline_watcher.ts
+++ watcher/kline_watcher.ts
@@ -1,4 +1,5 @@
 import { Exchange } from 'ccxt';
+import { Bot } from '../bot';
 import { ArrayToKLine, OHLCV } from '../tc/ohlcv';
 
 export
@@ -8,27 +9,29 @@ class KLineWatcher {
     symbol: string,
     timeframe: string,
     limit: number,
+    bot?: Bot<any, any, any>,
   ) {
     const data = await exchange.fetchOHLCV(symbol, timeframe, undefined, limit + 1);
     data.splice(data.length - 1, 1);
-    return ArrayToKLine(data);
+    const kline = ArrayToKLine(data);
+    kline.forEach((ohlcv) => bot?.Update(ohlcv, false));
+    return kline;
   }
 
   public async Start(
     exchange: Exchange,
     symbol: string,
     timeframe: string,
-    limit: number,
     callback: (kline: OHLCV[]) => void,
     interval = 1000,
   ) {
     try {
-      callback(await this.Fetch(exchange, symbol, timeframe, limit));
+      callback(await this.Fetch(exchange, symbol, timeframe, 1));
     } catch (e) {
       console.log(e);
     } finally {
       setTimeout(() => {
-        this.Start(exchange, symbol, timeframe, limit, callback, interval);
+        this.Start(exchange, symbol, timeframe, callback, interval);
       }, interval);
     }
   }
