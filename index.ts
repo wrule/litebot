48068f7da230a961f2bb67c213cd0d2279f0dc3e
jimao <jimao@jimaodeMBP.lan>
2022-11-18 23:02:03 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -3,26 +3,21 @@ import { binance } from 'ccxt';
 import { SMACross } from './bot/sma_cross';
 import { SimpleSpot } from './executor/simple_spot';
 import { KLineWatcherLite } from './watcher/kline_watcher_lite';
-import { App } from './app';
+import { fill_params } from './app';
 
-class X
-extends App {
-  public async Run() {
-    const params = {
-      symbol: 'ETH/USDT',
-      timeframe: '1m',
-      fast_period: 10,
-      slow_period: 40,
-      interval: 0,
-    };
-    this.fill_params(params);
-    const exchange = new binance({ });
-    console.log('loading market...');
-    await exchange.loadMarkets();
-    const executor = new SimpleSpot(100, 0.001);
-    const bot = new SMACross(executor, params);
-    new KLineWatcherLite().RunBot(exchange, params.symbol, params.timeframe, bot, params.interval);
-  }
-}
-
-new X().Run();
+(async () => {
+  const params = {
+    symbol: 'ETH/USDT',
+    timeframe: '1m',
+    fast_period: 10,
+    slow_period: 40,
+    interval: 0,
+  };
+  fill_params(params);
+  const exchange = new binance({ });
+  console.log('loading market...');
+  await exchange.loadMarkets();
+  const executor = new SimpleSpot(100, 0.001);
+  const bot = new SMACross(executor, params);
+  new KLineWatcherLite().RunBot(exchange, params.symbol, params.timeframe, bot, params.interval);
+})();
