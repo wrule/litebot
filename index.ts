8907ef2ed2751593c08556b43309ec0cbb143437
jimao <jimao@jimaodeMBP.lan>
2022-11-19 13:17:29 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -2,20 +2,20 @@ import { binance } from 'ccxt';
 import { RealSpot } from './executor/real_spot';
 import { DingTalk } from './notifier/dingtalk';
 
-const dingtalk = require('./.dingtalk.json');
 const secret = require('./.secret.json');
 
 async function main() {
-  const exchange = new binance(secret);
-  const notifier = new DingTalk(dingtalk);
-  const spot = new RealSpot({
-    exchange, notifier,
-    name: '测试',
-    symbol: 'BTC/USDT',
-    init_funds: 15,
-    init_assets: 0,
-  });
-  spot.BuyAll(1);
+  // const exchange = new binance(secret.exchange);
+  const notifier = new DingTalk(secret.notifier);
+  notifier.SendMessage('你好，世界');
+  // const spot = new RealSpot({
+  //   exchange, notifier,
+  //   name: '测试',
+  //   symbol: 'BTC/USDT',
+  //   init_funds: 15,
+  //   init_assets: 0,
+  // });
+  // spot.BuyAll(1);
 }
 
 main();
