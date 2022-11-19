16543266b16019328d79c7de6681468ffdced32b
jimao <jimao@jimaodeMBP.lan>
2022-11-19 21:34:09 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
-import { rsi, stoch, stoch_rsi } from './tulind_wrapper';
+import { ArrayToKLine } from './tc/ohlcv';
+import { stoch_rsi } from './tulind_wrapper';
+import moment from 'moment';
 
-let data = [2, 1, 2, 3, 4, 5, 6, 5, 4, 6, 6, 4, 4, 3, 4, 7, 7, 7, 7, 8];
-const result = stoch_rsi(data, { rsi_period: 3, stoch_period: 3, k_period: 2, d_period: 2 });
-console.log(result);
+const data = require('./data/ETH_USDT-2h.json');
+const kline = ArrayToKLine(data);
+const close = kline.map((item) => item.close);
+const { k, d, diff } = stoch_rsi(close, {
+  rsi_period: 13,
+  stoch_period: 45,
+  k_period: 32,
+  d_period: 45,
+});
+console.log(close.length, k.length);
+console.log(diff.slice(diff.length - 10));
+console.log(moment(new Date(kline[kline.length - 1].time)).format('YYYY-MM-DD HH:mm:ss'));
