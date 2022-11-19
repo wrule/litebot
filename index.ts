da69c3770a5f0dd3b283a4a09e97a36d5f2c9c61
jimao <jimao@jimaodeMBP.lan>
2022-11-19 21:16:21 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-import { rsi, stoch } from './tulind_wrapper';
+import { rsi, stoch, stoch_rsi } from './tulind_wrapper';
 
-let data = rsi([2, 1, 2, 3, 4, 5, 6, 5, 4, 6, 6, 4, 4, 3, 4, 7, 7, 7, 7, 8], 3);
-data = data.filter((item) => item);
-const result = stoch(data, data, data, { stoch_period: 3, k_period: 2, d_period: 2 });
+let data = [2, 1, 2, 3, 4, 5, 6, 5, 4, 6, 6, 4, 4, 3, 4, 7, 7, 7, 7, 8];
+const result = stoch_rsi(data, { rsi_period: 3, stoch_period: 3, k_period: 2, d_period: 2 });
 console.log(result);
