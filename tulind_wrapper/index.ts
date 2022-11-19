d562f97ed67d305ec9eedadd6fdb2bdd92b5eb69
jimao <jimao@jimaodeMBP.lan>
2022-11-19 20:13:55 +0800

    rsi

--- tulind_wrapper/index.ts
+++ tulind_wrapper/index.ts
@@ -9,3 +9,13 @@ function sma(source: number[], period: number) {
   });
   return result;
 }
+
+export
+function rsi(source: number[], period: number) {
+  let result: number[] = [];
+  tulind.indicators.rsi.indicator([source], [period], (error: any, data: any) => {
+    if (error) throw error;
+    result = Array(tulind.indicators.rsi.start([period])).fill(NaN).concat(data[0]);
+  });
+  return result;
+}
