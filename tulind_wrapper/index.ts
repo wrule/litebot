3d0cd0e9c5822094b31631d52e71852d0f55da71
jimao <jimao@jimaodeMBP.lan>
2022-11-19 20:42:23 +0800

    stoch

--- tulind_wrapper/index.ts
+++ tulind_wrapper/index.ts
@@ -19,3 +19,21 @@ function rsi(source: number[], period: number) {
   });
   return result;
 }
+
+export
+function stoch(high: number[], low: number[], close: number[], options: {
+  stoch_period: number,
+  k_period: number,
+  d_period: number,
+}) {
+  let k: number[] = [], d: number[] = [], diff: number[] = [];
+  const options_array = [options.stoch_period, options.k_period, options.d_period];
+  const start = tulind.indicators.stoch.start(options_array);
+  tulind.indicators.stoch.indicator([high, low, close], options_array, (error: any, data: any) => {
+    if (error) throw error;
+    k = Array(start).fill(NaN).concat(data[0]);
+    d = Array(start).fill(NaN).concat(data[1]);
+    diff = k.map((item, index) => item - d[index]);
+  });
+  return { k, d, diff };
+}
