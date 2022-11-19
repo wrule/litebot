cfdb2c894919c16b5f814878527933f911054821
jimao <jimao@jimaodeMBP.lan>
2022-11-19 13:42:44 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -5,17 +5,16 @@ import { DingTalk } from './notifier/dingtalk';
 const secret = require('./.secret.json');
 
 async function main() {
-  // const exchange = new binance(secret.exchange);
+  const exchange = new binance(secret.exchange);
   const notifier = new DingTalk(secret.notifier);
-  notifier.SendMessage('你好，世界');
-  // const spot = new RealSpot({
-  //   exchange, notifier,
-  //   name: '测试',
-  //   symbol: 'BTC/USDT',
-  //   init_funds: 15,
-  //   init_assets: 0,
-  // });
-  // spot.BuyAll(1);
+  const spot = new RealSpot({
+    exchange, notifier,
+    name: '测试',
+    symbol: 'BTC/USDT',
+    init_funds: 15,
+    init_assets: 0,
+  });
+  spot.BuyAll(1);
 }
 
 main();
