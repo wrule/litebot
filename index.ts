8dbe5ec03e708fc66505a44c15e89516d45b8626
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-21 16:32:08 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
-import { stoch } from './tulind_wrapper';
+import { stoch, stoch_rsi, stoch_rsi_start } from './tulind_wrapper';
 
 async function main() {
-  const close: number[] = [NaN, NaN, NaN];
-  const data = stoch(close, close, close, {
+  const close: number[] = [];
+  const data = stoch_rsi(close, {
+    rsi_period: 2,
     stoch_period: 2,
     k_period: 2,
     d_period: 2,
   });
   console.log(data);
+  console.log(stoch_rsi_start({
+    rsi_period: 2,
+    stoch_period: 2,
+    k_period: 2,
+    d_period: 2,
+  }));
 }
 
 main();
