0437d3c501ae95a6bad733ce1f537e1279eaeee5
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-21 14:24:49 +0800

    修改sma

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { stoch } from './tulind_wrapper';
 
 async function main() {
-  const close: number[] = [1, 2, 3, 4, 4, 5];
+  const close: number[] = [NaN, NaN, NaN];
   const data = stoch(close, close, close, {
     stoch_period: 2,
     k_period: 2,
