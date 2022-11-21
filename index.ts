e934a2f13f770e925eedc5bd75db492b72817bbd
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-21 14:18:14 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
-import { rsi } from './tulind_wrapper';
+import { stoch } from './tulind_wrapper';
 
 async function main() {
-  const close: number[] = [1, 1, 3, 2, 2, 7, 8];
-  const data = rsi(close, 4)
+  const close: number[] = [1, 2, 3, 4, 4, 5];
+  const data = stoch(close, close, close, {
+    stoch_period: 2,
+    k_period: 2,
+    d_period: 2,
+  });
   console.log(data);
 }
 
