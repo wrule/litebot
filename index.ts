50f6b217fc126842ff0f53314faa5144ecc8a335
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 18:22:27 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
-import { rsi, rsi_start, stoch } from 'tulind-wrapper';
+import moment from 'moment';
+import { rsi, stoch } from 'tulind-wrapper';
+import { ArrayToKLine } from './tc/ohlcv';
+
+const data = require('./data/ETH_USDT-30m.json');
 
 async function main() {
-  const close = [2, 2, 3, 1, 9, 2, 8, 9, 10, 11, 10, 9, 9, 10, 9, 9, 1, 9, 1, 9, 8];
-  const rsi_result = rsi(close, { period: 3 });
+  const kline = ArrayToKLine(data);
+  const close = kline.map((item) => item.close);
+  const rsi_result = rsi(close, { period: 13 });
   const stoch_result = stoch(rsi_result, rsi_result, rsi_result, {
-    k_period: 2,
-    k_slowing_period: 2,
-    d_period: 3,
+    k_period: 45,
+    k_slowing_period: 32,
+    d_period: 45,
   }, close.length);
-  console.log(stoch_result.stoch_k);
+  console.log(stoch_result.stoch_k.slice(stoch_result.stoch_k.length - 10));
+  console.log(moment(new Date(kline[kline.length - 1].time)).format('YYYY-MM-DD HH:mm:ss'));
 }
 
 main();
