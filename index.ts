0289dbc5c69d5e1d7a3aed9b9abb0e7068de7d33
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-23 12:24:59 +0800

    测试代码

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import { binance } from "ccxt";
-import { RealSpot } from "./executor/real_spot";
+import { binance } from 'ccxt';
+import { RealSpot } from './executor/real_spot';
+import { DingTalk } from './notifier/dingtalk';
 
 const secret = require('./.secret.json');
 
 async function main() {
   const exchange = new binance(secret.exchange);
+  const notifier = new DingTalk(secret.notifier);
   console.log('加载市场...');
   await exchange.loadMarkets();
   const spot = new RealSpot({
-    exchange,
+    exchange, notifier,
     symbol: 'ETH/BUSD',
     funds: 1e6,
   });
