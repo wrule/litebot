4e1365e4a6b633bae5fa3fdd5ed88b21c07a53e4
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-23 12:13:42 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
+import { binance } from "ccxt";
+import { RealSpot } from "./executor/real_spot";
 
-console.log('你好，世界');
+const secret = require('./.secret.json');
+
+async function main() {
+  const exchange = new binance(secret.exchange);
+  console.log('加载市场...');
+  await exchange.loadMarkets();
+  const spot = new RealSpot({
+    exchange,
+    symbol: 'ETH/BUSD',
+    funds: 1e6,
+  });
+  console.log('交易');
+  await spot.BuyAll(1200);
+}
+
+main();
