f3d0cbbe9f079d0f30893c570bf2dd1e81a4bb50
jimao <jimao@jimaodeMBP.lan>
2022-11-26 14:36:58 +0800

    红眼

--- bot/stoch_rsi_cross.ts
+++ bot/stoch_rsi_cross.ts
@@ -1,7 +1,13 @@
+#!/usr/bin/env node
 import { rsi, stoch, rsi_start, stoch_start } from 'tulind-wrapper';
 import { TC } from '../tc';
 import { Bot } from '.';
 import { FullSpot } from '../executor/full_spot';
+import { fill_params } from '../app';
+import { DingTalk } from '../notifier/dingtalk';
+import { binance } from 'ccxt';
+import { RealSpot } from '../executor/real_spot';
+import { KLineWatcher } from '../watcher/kline_watcher';
 
 export
 interface Signal
@@ -63,3 +69,29 @@ extends Bot<TC, Signal> {
     }
   }
 }
+
+(async () => {
+  if (require.main !== module) return;
+  const secret = require('../.secret.json');
+  const params = {
+    name: '红眼',
+    symbol: 'ETH/USDT',
+    timeframe: '1m',
+    rsi_period: 2,
+    stoch_period: 3,
+    k_period: 4,
+    d_period: 5,
+    interval: 0,
+    funds: 15,
+    assets: 0,
+  };
+  fill_params(params);
+  const notifier = new DingTalk(secret.notifier);
+  const exchange = new binance(secret.exchange);
+  console.log('loading market...');
+  await exchange.loadMarkets();
+  const executor = new RealSpot({ exchange, notifier, ...params });
+  // const executor = new SimpleSpot();
+  const bot = new StochRSICross(executor, params);
+  new KLineWatcher().RunBot({ exchange, bot, ...params });
+})();
