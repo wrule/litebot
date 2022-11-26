d907ce37e79fbb6d08d86317cd7d766648aaa06a
jimao <jimao@jimaodeMBP.lan>
2022-11-26 22:15:52 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { ArrayToKLine } from './tc/ohlcv';
 
 async function main() {
   const data = require('../data/ETH_USDT-30m.json');
-  const kline = ArrayToKLine(data, 1e3 * 60 * 30);
+  const kline = ArrayToKLine(data);
   console.log('你好，世界', kline[kline.length - 1].close);
 }
 
