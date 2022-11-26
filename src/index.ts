849b68b76ec686afa50948e9191b4bda54a63742
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-26 23:08:49 +0800

    删除需要的代码

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import { ArrayToKLine } from './tc/ohlcv';
-
 async function main() {
-  const data = require('../data/ETH_USDT-30m.json');
-  const kline = ArrayToKLine(data);
-  console.log('你好，世界', kline[kline.length - 1].close);
+
 }
 
 main();
