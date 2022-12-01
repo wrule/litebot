f46c31d0803464e4dc21b0e16df040d1f1fbd083
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-01 16:29:23 +0800

    测试文件

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import { ArrayToKLine, FillHighFirst } from './tc/ohlcv';
+import { ArrayToKLine, ExpandKLine } from './tc/ohlcv';
 import fs from 'fs';
 
 const data1 = require('../src/data/ETH_USDT-30m.json');
@@ -8,10 +8,7 @@ const kline2 = ArrayToKLine(data2, false);
 
 function main() {
   console.log('你好世界');
-  FillHighFirst(kline1, kline2);
-  console.log(kline1.every((item) => item.high_first != null));
-  fs.writeFileSync('output1.json', JSON.stringify(kline1, null, 2));
-  fs.writeFileSync('output2.json', JSON.stringify(kline2, null, 2));
+  ExpandKLine(kline1);
 }
 
 main();
