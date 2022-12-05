6c713528c77b563efc9a273c8b6220bc45100b3a
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-05 11:08:08 +0800

    测试代码

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,18 @@
 
-import { ccxt, SpotSimpleTest } from '.';
+import { ArrayToKLine, ccxt, SpotSimpleTest } from '.';
 import { KLineWatcherRT } from './watcher/kline_watcher_rt';
 
 const secret = require('../.secret.json');
+const data = require('../src/data/ETH_USDT-30m.json');
 
 function main() {
-  const exchange = new ccxt.binance(secret.exchange);
-  const watcher = new KLineWatcherRT();
-  watcher.start(exchange, 'ETH/USDT', '1m', (data) => {
-    const [hist, active] = data;
-    console.log(hist.close, active.close);
-  });
+  const kline = ArrayToKLine(data);
+  // const exchange = new ccxt.binance(secret.exchange);
+  // const watcher = new KLineWatcherRT();
+  // watcher.start(exchange, 'ETH/USDT', '1m', (data) => {
+  //   const [hist, active] = data;
+  //   console.log(hist.close, active.close);
+  // });
 }
 
 main();
