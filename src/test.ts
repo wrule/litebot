88a9d2057b57be043e31ce82a022db86f2e00308
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-12 12:09:15 +0800

    删除不需要的代码

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@ import { ArrayToKLine, ccxt, SpotSimpleTest } from '.';
 import { KLineWatcherRT } from './watcher/kline_watcher_rt';
 
 const secret = require('../.secret.json');
-const data = require('../src/data/ETH_USDT-30m.json');
+// const data = require('../src/data/ETH_USDT-30m.json');
 
 async function main() {
   const exchange = new ccxt.binance(secret.exchange);
