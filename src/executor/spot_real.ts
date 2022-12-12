b50f02be3409dcb462b857df1626d9608642b736
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-12 12:27:36 +0800

    roi百分比输出

--- src/executor/spot_real.ts
+++ src/executor/spot_real.ts
@@ -46,7 +46,7 @@ class SpotReal {
       in_amount: in_out[0], out_amount: in_out[1],
       expected_price: price, final_price: order.price, deviation: `${(order.price - price) / price * 100}%`,
       funds: this.funds, assets: this.assets,
-      valuation: this.Valuation(order.price), roi: this.ROI(order.price),
+      valuation: this.Valuation(order.price), roi: `${this.ROI(order.price) * 100}%`,
       order_time,
     };
     console.log(message);
