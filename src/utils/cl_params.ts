b98f54ed7c097678524a3b9aed0988d69f2410ca
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-13 18:05:23 +0800

    删除改造不需要的代码

--- src/utils/cl_params.ts
+++ src/utils/cl_params.ts
@@ -1,20 +1,12 @@
 // 2022年12月08日23:42:46
 import * as args from '@wrule/args';
 
-function params_command_line_view(params: any) {
-  const cp = { ...params };
-  delete cp['_'];
-  const script_name = cp['$0'];
-  delete cp['$0'];
-  return `./${script_name} ${Object.entries(cp).map(([key, value]) => `--${key} ${value}`).join(' ')}`;
-}
-
 export
 function FillParams(params: any) {
   Object.entries(args.parse(process.argv)).forEach(([key, value]) => {
     if (!(key in params)) throw `unknown param ${key}`;
     params[key] = value;
   });
-  console.log(params_command_line_view(params));
+  console.log(`./${args.file_name(process.argv[1])} ${args.unparse_str(params)}`);
   console.log(params);
 }
