29bb4bdbbc649115ddc288f3288488eeafe4375a
jimao <jimao@jimaodeMBP.lan>
2023-01-14 13:55:27 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export { Domain, Value, RandomSelect, Option, Random } from './optimizer/random'
 export { TC } from './tc';
 export { OHLCV, ArrayToOHLCV, ArrayToKLine, ExpandKLine } from './tc/ohlcv';
 export { FillParams } from './utils/cl_params';
+export { ExFactoryConfig, ExFactory } from './utils/ex_factory';
 export { KLineWatcher, TimeframeToMS } from './watcher/kline_watcher';
 export { KLineWatcherRT } from './watcher/kline_watcher_rt';
 export * as ccxt from 'ccxt';
