ad68d670045a8971bab4a23ea3e1c95929cd222b
jimao <jimao@jimaodeMBP.lan>
2023-01-15 15:55:46 +0800

    错误处理

--- src/executor/spot_real.ts
+++ src/executor/spot_real.ts
@@ -89,8 +89,12 @@ class SpotReal {
             'quote_ccy' : undefined,
         } as any,
       );
-      this.config.exchange.id === 'okx' &&
-        (order = await this.config.exchange.fetchOrder(order.id, order.symbol));
+      try {
+        this.config.exchange.id === 'okx' &&
+          (order = await this.config.exchange.fetchOrder(order.id, order.symbol));
+      } catch (e) {
+        console.log(e);
+      }
       const order_time = `${(Number(new Date()) - request_time) / 1000}s`;
       const in_amount = order.cost;
       const out_amount = order.amount - (this.config.symbol.startsWith(order.fee?.currency) ? order.fee.cost : 0);
@@ -118,8 +122,12 @@ class SpotReal {
         this.config.symbol,
         this.config.exchange.amountToPrecision(this.config.symbol, this.assets),
       );
-      this.config.exchange.id === 'okx' &&
-        (order = await this.config.exchange.fetchOrder(order.id, order.symbol));
+      try {
+        this.config.exchange.id === 'okx' &&
+          (order = await this.config.exchange.fetchOrder(order.id, order.symbol));
+      } catch (e) {
+        console.log(e);
+      }
       const order_time = `${(Number(new Date()) - request_time) / 1000}s`;
       const yield_rate = `${(order.price - final_price) / final_price * 100}%`;
       const in_amount = order.amount;
