891f42ebec08cbe00cd26d74f396ff62ea192e83
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-18 17:07:01 +0800

    加载市场

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,10 @@
+import { ExFactory } from './utils/ex_factory';
+
+const secret = require('../.secret.json');
 
 async function main() {
+  const exchange = ExFactory(secret.exchange);
+  await exchange.loadMarkets();
   console.log('你好，世界');
 }
 
