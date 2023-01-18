825f2ef8c409be22429cf830f2f7e9f02e7003bd
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-18 17:20:16 +0800

    代理

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,14 @@
+import HttpsProxyAgent from 'https-proxy-agent';
 import { ExFactory } from './utils/ex_factory';
 
 const secret = require('../.secret.json');
 
 async function main() {
-  const exchange = ExFactory(secret.exchange);
+  const exchange = ExFactory({
+    ...secret.exchange,
+    agent: secret.exchange.https_proxy_agent ?
+      HttpsProxyAgent(secret.exchange.https_proxy_agent) : undefined,
+  });
   await exchange.loadMarkets();
   console.log('你好，世界');
 }
