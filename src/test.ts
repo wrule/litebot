8cec993b8426fc63a6d2422ce8fa2da51a5985b1
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-18 17:32:12 +0800

    获取期货合约市场

--- src/test.ts
+++ src/test.ts
@@ -6,11 +6,18 @@ const secret = require('../.secret.json');
 async function main() {
   const exchange = ExFactory({
     ...secret.exchange,
+    options: {
+      defaultType: 'future',
+      hedgeMode: true,
+    },
     agent: secret.exchange.https_proxy_agent ?
       HttpsProxyAgent(secret.exchange.https_proxy_agent) : undefined,
   });
-  await exchange.loadMarkets();
+  const markets = await exchange.loadMarkets();
   console.log('你好，世界');
+  Object.entries(markets).forEach(([key, value]) => {
+    console.log(key);
+  });
 }
 
 main();
