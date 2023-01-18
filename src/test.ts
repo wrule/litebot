f350117570b2aed606e52a33c4aa8cba724435f7
jimao <jimao@jimaodeMacBook-Pro.local>
2023-01-18 16:28:17 +0800

    准备实验性代码

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,6 @@
 
-import { ArrayToKLine, ccxt, SpotSimpleTest } from '.';
-import { KLineWatcherRT } from './watcher/kline_watcher_rt';
-
-const secret = require('../.secret.json');
-// const data = require('../src/data/ETH_USDT-30m.json');
-
 async function main() {
-  const exchange = new ccxt.binance(secret.exchange);
-  const ticker = await exchange.fetchTicker('ETH/USDT');
-  console.log(ticker);
+  console.log('你好，世界');
 }
 
 main();
